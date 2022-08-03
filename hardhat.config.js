cd52850bde67d8a063664328bf81099f9bf6aff4
Flavio Brassesco <brassescoflavio@gmail.com>
2022-08-03 17:23:18 -0300

    initial

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,25 @@
+require("@typechain/hardhat");
 require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.9",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.9",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 999999,
+          },
+        },
+      },
+    ],
+  },
+  typechain: {
+    outDir: "types",
+    target: "ethers-v5",
+    alwaysGenerateOverloads: false, // should overloads with full signatures like deposit(uint256) be generated always, even if there are no overloads?
+    dontOverrideCompile: false, // defaults to false
+  },
 };
